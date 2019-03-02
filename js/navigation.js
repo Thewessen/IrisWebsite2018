0c504a38b0931c668a26f5a4cdca0fe8395f43ff
Thewessen <sthewessen@gmail.com>
2019-03-02 15:04:24 +0100

    New branch for the old website. It might have some use later.

--- js/navigation.js
+++ js/navigation.js
@@ -4,9 +4,6 @@
         link:   './index.html',
         content:'Iris van der Ende'
     },{
-        link:   './nieuw.html',
-        content:'NIEUW!!'
-    },{
         link:   './onder_de_brug.html',
         content:'project | Onder de brug'
     },{
